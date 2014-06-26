e1b59ef1069ba8105a8469754936e8bcad4d0fee
AlexSTV <alexcrafter@gmail.com>
2014-06-26 11:10:36 +0200

    Removed broken comments field, I'll update with a fix eventually

--- article.php
+++ article.php
@@ -1,63 +1,61 @@
 <?php theme_include('header'); ?>
 
-		<section class="content wrap" id="article-<?php echo article_id(); ?>">
-			<h1><?php echo article_title(); ?></h1>
+<?php 
+$in= article_markdown();
+$out = preg_replace_callback(
+    "(\[image\](.*?)\[/image\])is",
+    function($m) {
+        static $id = 0;
+        $id++;
+        return "</bodytext> </div> </div> <img src=".$m[1]." /> <div id=\"filler\"> <div class=\"description\"> <bodytext>";
+    },
+    $in);
+	?>
 
-			<article>
-				<?php echo article_markdown(); ?>
-			</article>
-
-			<section class="footnote">
-				<!-- Unfortunately, CSS means everything's got to be inline. -->
-				<p>This article is my <?php echo numeral(total_articles()); ?> oldest. It is <?php echo count_words(article_markdown()); ?> words long<?php if(comments_open()): ?>, and it’s got <?php echo total_comments() . pluralise(total_comments(), ' comment'); ?> for now.<?php endif; ?> <?php echo article_custom_field('attribution'); ?></p>
-			</section>
-		</section>
-
-		<?php if(comments_open()): ?>
-		<section class="comments">
-			<?php if(has_comments()): ?>
-			<ul class="commentlist">
-				<?php $i = 0; while(comments()): $i++; ?>
-				<li class="comment" id="comment-<?php echo comment_id(); ?>">
-					<div class="wrap">
-						<h2><?php echo comment_name(); ?></h2>
-						<time><?php echo relative_time(comment_time()); ?></time>
-
-						<div class="content">
-							<?php echo comment_text(); ?>
-						</div>
-
-						<span class="counter"><?php echo $i; ?></span>
+<div class="bg2">
+	<article class="wrapper" >
+<div class="post">
+				
+					<?php if(article_custom_field('image', '')!=""): ?>
+						<img src="<?php echo article_custom_field('image', ''); ?>" />
+					<?php else: ?>
+						<?php echo article_html(); ?>
+					<?php endif; ?>
+					
+					<?php if(article_custom_field('image', '') !=""): ?>
+					
+						<div id="filler">
+							<div class="description">
+								<bodytext>
+									<?php echo $out; ?>
+								</bodytext>
+							</div>
+						</div>	
+						
+					<?php else: ?>
+					
+						<?php if(article_description() !=""): ?>
+							<div id="filler">
+								<div class="description">
+									<bodytext>
+										<?php echo article_description(); ?>
+									</bodytext>
+								</div>
+							</div>
+						<?php endif; ?>
+						
+					<?php endif; ?>
+					<section class="yspace" ></section>
+					<div class="datestamp">
+						<stamptext><?php echo article_date(); ?></stamptext>
 					</div>
-				</li>
-				<?php endwhile; ?>
-			</ul>
-			<?php endif; ?>
+					<div class="authorstamp">
+						<stamptext><?php echo article_author(); ?></stamptext>
+					</div>
+					<section class="yspace" ></section>
+				</div>
+</article>
+	</div>
 
-			<form id="comment" class="commentform wrap" method="post" action="<?php echo comment_form_url(); ?>#comment">
-				<?php echo comment_form_notifications(); ?>
-
-				<p class="name">
-					<label for="name">Your name:</label>
-					<?php echo comment_form_input_name('placeholder="Your name"'); ?>
-				</p>
-
-				<p class="email">
-					<label for="email">Your email address:</label>
-					<?php echo comment_form_input_email('placeholder="Your email (won’t be published)"'); ?>
-				</p>
-
-				<p class="textarea">
-					<label for="text">Your comment:</label>
-					<?php echo comment_form_input_text('placeholder="Your comment"'); ?>
-				</p>
-
-				<p class="submit">
-					<?php echo comment_form_button(); ?>
-				</p>
-			</form>
-
-		</section>
-		<?php endif; ?>
 
 <?php theme_include('footer'); ?>
